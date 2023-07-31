c17d79e67d1cb1430b4585f2219c7e0bce657e04
Vincent Yang <vincentyangoz@gmail.com>
2023-07-31 13:19:44 +1000

    remove unused code

--- test-image.py
+++ test-image.py
@@ -1,40 +1,14 @@
 import os
 import sys
 # evaluate the deep model on the test dataset
-from keras.datasets import cifar10
-from keras.models import load_model
-from keras.utils import to_categorical
-import numpy as np
-
 # make a prediction for a new image.
 from keras.preprocessing.image import load_img
 from keras.preprocessing.image import img_to_array
 from keras.models import load_model
-
 from datetime import datetime
 
 def curr_time():
 	return datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
-
-# load train and test dataset
-def load_dataset():
-	# load dataset
-	(trainX, trainY), (testX, testY) = cifar10.load_data()
-	# one hot encode target values
-	trainY = to_categorical(trainY)
-	testY = to_categorical(testY)
-	return trainX, trainY, testX, testY
-
-# scale pixels
-def prep_pixels(train, test):
-	# convert from integers to floats
-	train_norm = train.astype('float32')
-	test_norm = test.astype('float32')
-	# normalize to range 0-1
-	train_norm = train_norm / 255.0
-	test_norm = test_norm / 255.0
-	# return normalized images
-	return train_norm, test_norm
 
 # load and prepare the image
 def load_image(filename):
@@ -86,7 +60,7 @@ else:
 	
 file_log = file_image + ".log"
 f=open(file_log, "at")
-msg = "{}: running file:{} on the model:{}\n".format(curr_time(), file_image, file_model)
+msg = "{}: running image file:{} on the model:{}\n".format(curr_time(), file_image, file_model)
 print(msg)
 f.write(msg)
 result = test_model(file_model, file_image)
